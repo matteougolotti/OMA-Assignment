2302657c9771bf895fd642b659bafd55037e2e86
matteo.ugolotti <matteo.ugolotti@gmail.com>
2014-11-19 16:04:11 +0100

    Added GA class

---
+++ src/com/mdvrp/GAInitialSolution.java
@@ -0,0 +1,30 @@
+package com.mdvrp;
+
+import com.softtechdesign.ga.Crossover;
+import com.softtechdesign.ga.GAException;
+import com.softtechdesign.ga.GASequenceList;
+
+public class GAInitialSolution extends GASequenceList {
+
+	public GAInitialSolution() throws GAException {
+		super(  20, //size of chromosome
+                300, //population has N chromosomes
+                0.7, //crossover probability
+                10, //random selection chance % (regardless of fitness)
+                2000, //max generations
+                0, //num prelim runs (to build good breeding stock for final/full run)
+                25, //max generations per prelim run
+                0.06, //chromosome mutation prob.
+                0, //number of decimal places in chrom
+                "ABCDEFGHIJKLMNOPQRST", //gene space (possible gene values)
+                Crossover.ctTwoPoint, //crossover type
+                true); //compute statisitics?
+	}
+
+	@Override
+	protected double getFitness(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
