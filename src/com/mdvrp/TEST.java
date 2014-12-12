4a644deca44241c73ce2c6aafec347ae24687e20
filippo.balla <filippo.balla@gmail.com>
2014-12-12 13:37:29 +0100

    test

--- src/com/mdvrp/TEST.java
+++ src/com/mdvrp/TEST.java
@@ -12,8 +12,46 @@ private static Duration d = new Duration();
 			
 			long minutes=0;
 			long seconds=0;
+			
+			int n = args.length/2;
 		
 			d.start();
+			for(int j = 0; j<n; j+=2)
+			{
+				for(int i = 0; i < MAX; i++)
+				{
+					String[] file = new String[2];
+					file[0]="-if";
+					file[1]=args[j+1];
+					MDVRPTWGA.main(file);
+					d.stop();
+					seconds += d.getSeconds();
+					if (seconds>=60){
+						long m = seconds/60;
+						seconds-=60*m;
+						minutes+=m;
+					}
+						
+					System.out.println("IT -> "+(i+1)+" elapsed time : "+minutes+"."+seconds+"''");
+					d.start();
+				}
+				
+				d.stop();
+				minutes += d.getMinutes();
+				seconds += d.getSeconds();
+				String outSol = "ANALISI :";
+				Parameters parameters = MDVRPTWGA.instance.getParameters();
+		        FileWriter fw = null;
+				try {
+					fw = new FileWriter(parameters.getOutputFileName(),true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        fw.write(outSol);
+		        fw.close();
+				System.out.println("FINE."+" elapsed time : "+minutes+"."+seconds+"''");
+			}
 			for(int i = 0; i < MAX; i++)
 			{
 				MDVRPTWGA.main(args);
